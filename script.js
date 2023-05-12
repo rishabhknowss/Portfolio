858d04bbe2b8441b79f21dae83487879de7e7037
RISHABH RAI <rishabhrai1515@gmail.com>
2023-05-12 19:09:10 +0530

    typing animation added

--- script.js
+++ script.js
@@ -9,20 +9,38 @@ document.addEventListener('DOMContentLoaded', function() {
     inputElement.addEventListener('keydown', function(event) {
       if (event.key === 'Enter') {
         generateResponse(inputElement.value);
+        inputElement.value = ''; // Clear the input text
       }
     });
   
     // Add event listener for icon click
     iconElement.addEventListener('click', function() {
       generateResponse(inputElement.value);
+      inputElement.value = ''; // Clear the input text
     });
   
     // Function to generate response based on input
     function generateResponse(text) {
       if (text.trim() !== '') {
         var response = "Hi, what do you wanna know?";
-        document.getElementById('response-text').textContent = response;
+        animateTyping(response, 'response-text');
       }
+    }
+  
+    // Function to animate typing effect
+    function animateTyping(text, elementId) {
+      var element = document.getElementById(elementId);
+      element.textContent = ''; // Clear previous content
+  
+      var i = 0;
+      var typingEffect = setInterval(function() {
+        element.textContent += text.charAt(i);
+        i++;
+  
+        if (i >= text.length) {
+          clearInterval(typingEffect);
+        }
+      }, 50); // Adjust typing speed here (milliseconds)
     }
   });
   
