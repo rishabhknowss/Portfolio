a54e929de49662e6c47f66577db622121e875bcc
rishabhknowss <rishabhrai1515@gmail.com>
2024-02-12 00:13:35 +0530

    img issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import { useNavigate } from "react-router-dom"
+import logo from "./assets/pfp.png"
 
 export const Navbar = ()=>{
     const navigate = useNavigate();
     return (
         <>
         <div className="flex rounded-md shadow-md bg-white p-5 text-2xl">
-            <div className="rounded-full overflow-hidden h-12 w-12 mr-4"><img src="src/assets/images/pfp.png" alt="pfp" ></img></div>
+            <div className="rounded-full overflow-hidden h-12 w-12 mr-4"><img src={logo} alt="pfp" ></img></div>
             <div className="font-medium m-4 cursor-pointer" onClick={()=>{navigate('/')}}>About</div>
             <div className="font-medium m-4 cursor-pointer" onClick={()=>{navigate('/hackathons')}}>Hackathons</div>
             <div className="font-medium m-4 cursor-pointer" onClick={()=>{navigate('/projects')}}>Projects</div>
