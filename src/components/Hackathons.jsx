f35651183e0e046c9030e94998442c33b8432c3c
rishabhknowss <rishabhrai1515@gmail.com>
2024-02-12 01:48:08 +0530

    changes

--- src/components/Hackathons.jsx
+++ src/components/Hackathons.jsx
@@ -11,13 +11,14 @@ export const Hackathons = () => {
         <div className="mt-5">EduHacks 2024<a href="https://devpost.com/software/quinelaw"><span className="ml-4 font-bold underline">Project</span></a></div>
         <div className="mt-5">HackSharks 2024<a href="https://devpost.com/software/ezcodegen"><span className="ml-4 font-bold underline">Project</span></a></div>
         <div className="mt-5 font-bold text-2xl">2023</div>
+        <div className="mt-5 "> EthOnline  2023 <a href="https://github.com/rishabhknowss/salaryfi"><span className="ml-4 font-bold underline">Project</span></a></div>
         <div className="mt-5 "> hackCBS 2023 <a href="https://devfolio.co/projects/sangeetai-8cca"><span className="ml-4 font-bold underline">Project</span></a></div>
         <div className="mt-5 "> Ethforall  2023 <a href="https://devfolio.co/projects/doge-draws-af03"><span className="ml-4 font-bold underline">Project</span></a></div>
         <div className="mt-5 "> SIH - Smart India Hackathon  2023<a href="https://github.com/rishabhknowss/certifix"><span className="ml-4 font-bold underline">Project</span></a></div>
         <div className="mt-5 font-bold text-2xl">2022</div>
         <div className="mt-5 flex "> hackCBS - India's Largest Student-run Hackathon  2022 <span className="text-yellow-700 font-bold">(Winner)</span><svg xmlns="http://www.w3.org/2000/svg" fill="yellow" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0" />
-</svg><a href="https://github.com/rishabhknowss/NoMic">
+</svg><a href="https://www.linkedin.com/posts/rishabhknows_hackcbs-hackathon2022-permissionless-activity-7000100334039650304-knYz?utm_source=share&utm_medium=member_desktop">
 <span className="ml-4 font-bold underline">Project</span></a></div>
         <div className="mt-5 "> hackVSIT 2022 <a href='https://devfolio.co/projects/trauma-waivers-e6a2'><span className="ml-4 font-bold underline">Project</span></a></div>
         
