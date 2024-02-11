97f2983f076e34138cd0ec3a2093a5847cda4f8d
rishabhknowss <rishabhrai1515@gmail.com>
2024-02-12 00:04:16 +0530

    img issue

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,7 +21,7 @@ export const About = ()=> {
         <Navbar></Navbar>
         <div className="flex justify-center mt-60">
         <div className="grid grid-cols-1 text-3xl ">
-        <div > Hi, I'm <span className="font-bold"> Rishabh Rai.</span> </div>
+        <div > Hi, I'm <span className="font-bold"> Rishabh Rai</span></div>
         <div className="mt-2">I build <span className="font-bold">{currentTech}</span> stuff.</div>
         <div className="mt-2">Connect on <a href='https://twitter.com/rishabhtwtsX'><span className="font-bold underline" >Twitter </span></a>&<a href='https://github.com/rishabhknowss'><span className="font-bold underline"> Github</span></a></div>
         </div>
