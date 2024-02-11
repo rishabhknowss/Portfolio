18ac596d44985fe03ead9dac7c779079a2a8a0b8
rishabhknowss <rishabhrai1515@gmail.com>
2024-02-12 02:57:50 +0530

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ export const About = ()=> {
     useEffect(() => {
         const intervalId = setInterval(() => {
         setTechIndex((prevIndex) => (prevIndex + 1) % techList.length);
-        }, 2000);
+        }, 1500);
 
         return () => clearInterval(intervalId);
     }, []);
@@ -19,11 +19,11 @@ export const About = ()=> {
     return (
         <>
         <Navbar></Navbar>
-        <div className="flex justify-center mt-60">
+        <div className="flex justify-center mx-auto my-40">
         <div className="grid grid-cols-1 text-3xl ">
-        <div > Hi, I'm <span className="font-bold"> Rishabh Rai</span></div>
+        <div > Hi, I'm <span className="font-bold"> Rishabh Rai.</span></div>
         <div className="mt-2">I build <span className="font-bold">{currentTech}</span> stuff.</div>
-        <div className="mt-2">Connect on <a href='https://twitter.com/rishabhtwtsX'><span className="font-bold underline" >Twitter </span></a>&<a href='https://github.com/rishabhknowss'><span className="font-bold underline"> Github</span></a></div>
+        <div className="mt-2">Connect on <a href='https://twitter.com/rishabhtwtsX'><span className="font-bold underline" >Twitter </span></a>&<a href='https://github.com/rishabhknowss'><span className="font-bold underline"> Github.</span></a></div>
         </div>
         </div>
         </>
