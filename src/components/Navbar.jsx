7f6ea33dc5a6477b0828f56adc64f50bc7fd28b5
rishabhknowss <rishabhrai1515@gmail.com>
2024-02-11 23:56:50 +0530

    image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export const Navbar = ()=>{
     return (
         <>
         <div className="flex rounded-md shadow-md bg-white p-5 text-2xl">
-            <div className="rounded-full overflow-hidden h-12 w-12 mr-4"><img src="src/assets/pfp.png"></img></div>
+            <div className="rounded-full overflow-hidden h-12 w-12 mr-4"><img src="src/assets/images/pfp.png"></img></div>
             <div className="font-medium m-4 cursor-pointer" onClick={()=>{navigate('/')}}>About</div>
             <div className="font-medium m-4 cursor-pointer" onClick={()=>{navigate('/hackathons')}}>Hackathons</div>
             <div className="font-medium m-4 cursor-pointer" onClick={()=>{navigate('/projects')}}>Projects</div>
