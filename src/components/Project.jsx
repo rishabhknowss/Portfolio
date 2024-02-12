2d4541258e689df4c3a413400e5a52535e13c04d
rishabhknowss <rishabhrai1515@gmail.com>
2024-02-12 13:15:35 +0530

    changes

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,6 +5,7 @@ export const Project = ()=>{
         <>
         <Navbar>
         </Navbar>
+        <h6 className="flex justify-center mt-10 text-gray-700">Coming Soon.</h6>
         </>
     )
 } 
